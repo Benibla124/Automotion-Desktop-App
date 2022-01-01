41562d582ed91f6f6343c6e0c2788c275b17c09c
Benjamin Blacher <schule@benjaminblacher.com>
2022-01-01 15:32:21 +0100

    GUI is there, including fullscreen toggle, Exit key, file opener (using system dialog) and Page switcher
    Signed-off-by: Benjamin Blacher <schule@benjaminblacher.com>

--- GUI/win_main.py
+++ GUI/win_main.py
@@ -16,14 +16,15 @@ from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
     QIcon, QImage, QKeySequence, QLinearGradient,
     QPainter, QPalette, QPixmap, QRadialGradient,
     QTransform)
-from PySide6.QtWidgets import (QApplication, QMainWindow, QMenu, QMenuBar,
-    QSizePolicy, QStatusBar, QWidget)
+from PySide6.QtWidgets import (QApplication, QHeaderView, QLabel, QMainWindow,
+    QMenu, QMenuBar, QSizePolicy, QStackedWidget,
+    QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget)
 
 class Ui_win_main(object):
     def setupUi(self, win_main):
         if not win_main.objectName():
             win_main.setObjectName(u"win_main")
-        win_main.resize(1273, 800)
+        win_main.resize(896, 784)
         self.actionBeenden = QAction(win_main)
         self.actionBeenden.setObjectName(u"actionBeenden")
         self.action_ffnen = QAction(win_main)
@@ -31,20 +32,51 @@ class Ui_win_main(object):
         self.actionVollbild = QAction(win_main)
         self.actionVollbild.setObjectName(u"actionVollbild")
         self.actionVollbild.setCheckable(True)
+        self.actionVollbild.setChecked(False)
+        self.actionOverview = QAction(win_main)
+        self.actionOverview.setObjectName(u"actionOverview")
+        self.actionTable_View = QAction(win_main)
+        self.actionTable_View.setObjectName(u"actionTable_View")
         self.centralwidget = QWidget(win_main)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout_2 = QVBoxLayout(self.centralwidget)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.pageswitcher = QStackedWidget(self.centralwidget)
+        self.pageswitcher.setObjectName(u"pageswitcher")
+        self.view_overview = QWidget()
+        self.view_overview.setObjectName(u"view_overview")
+        self.pageswitcher.addWidget(self.view_overview)
+        self.view_table = QWidget()
+        self.view_table.setObjectName(u"view_table")
+        self.verticalLayout_3 = QVBoxLayout(self.view_table)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.lab_tableview = QLabel(self.view_table)
+        self.lab_tableview.setObjectName(u"lab_tableview")
+        font = QFont()
+        font.setPointSize(18)
+        self.lab_tableview.setFont(font)
+        self.lab_tableview.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_3.addWidget(self.lab_tableview)
+
+        self.table_tableview = QTableWidget(self.view_table)
+        self.table_tableview.setObjectName(u"table_tableview")
+
+        self.verticalLayout_3.addWidget(self.table_tableview)
+
+        self.pageswitcher.addWidget(self.view_table)
+
+        self.verticalLayout_2.addWidget(self.pageswitcher)
+
         win_main.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(win_main)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 1273, 20))
+        self.menubar.setGeometry(QRect(0, 0, 896, 20))
         self.menuDatei = QMenu(self.menubar)
         self.menuDatei.setObjectName(u"menuDatei")
         self.menuFenster = QMenu(self.menubar)
         self.menuFenster.setObjectName(u"menuFenster")
         win_main.setMenuBar(self.menubar)
-        self.statusbar = QStatusBar(win_main)
-        self.statusbar.setObjectName(u"statusbar")
-        win_main.setStatusBar(self.statusbar)
 
         self.menubar.addAction(self.menuDatei.menuAction())
         self.menubar.addAction(self.menuFenster.menuAction())
@@ -52,6 +84,9 @@ class Ui_win_main(object):
         self.menuDatei.addSeparator()
         self.menuDatei.addAction(self.actionBeenden)
         self.menuFenster.addAction(self.actionVollbild)
+        self.menuFenster.addSeparator()
+        self.menuFenster.addAction(self.actionOverview)
+        self.menuFenster.addAction(self.actionTable_View)
 
         self.retranslateUi(win_main)
 
@@ -63,6 +98,9 @@ class Ui_win_main(object):
         self.actionBeenden.setText(QCoreApplication.translate("win_main", u"Beenden", None))
         self.action_ffnen.setText(QCoreApplication.translate("win_main", u"\u00d6ffnen", None))
         self.actionVollbild.setText(QCoreApplication.translate("win_main", u"Vollbild", None))
+        self.actionOverview.setText(QCoreApplication.translate("win_main", u"Overview", None))
+        self.actionTable_View.setText(QCoreApplication.translate("win_main", u"Table View", None))
+        self.lab_tableview.setText(QCoreApplication.translate("win_main", u"Table View", None))
         self.menuDatei.setTitle(QCoreApplication.translate("win_main", u"Datei", None))
         self.menuFenster.setTitle(QCoreApplication.translate("win_main", u"Fenster", None))
     # retranslateUi
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from PySide6.QtWidgets import QApplication, QMainWindow
+from csv import reader
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
 from GUI.win_main import Ui_win_main
-
 
 windowtitle = "RC-Car Viewer"
 
@@ -11,13 +11,24 @@ class WinMain(QMainWindow, Ui_win_main):
         self.setupUi(self)
         self.setWindowTitle(windowtitle)
         self.actionBeenden.triggered.connect(self.close)
+        self.action_ffnen.triggered.connect(self.openfile)
         self.actionVollbild.triggered.connect(self.toggle_fullscreen)
+        self.actionOverview.triggered.connect(lambda: self.pageswitcher.setCurrentIndex(0))
+        self.actionTable_View.triggered.connect(lambda: self.pageswitcher.setCurrentIndex(1))
 
     def toggle_fullscreen(self):
         if self.isFullScreen():
             self.showNormal()
         else:
             self.showFullScreen()
+
+    def openfile(self):
+        filepath = QFileDialog.getOpenFileName(self, self.tr("Open Data"), "/home", self.tr("*.txt *.csv"))
+        try:
+            datafile = open(filepath[0], 'r')
+            data = list(reader(datafile))
+        except:
+            print("Error reading file (No File selected?)")
 
 
 desktop_app = QApplication()
