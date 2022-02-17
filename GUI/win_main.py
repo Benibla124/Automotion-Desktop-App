6437cfc0b9a0c7dcec5d20c5d1b6db21b3dcbb45
Benjamin Blacher <privat@benjaminblacher.com>
2022-02-17 12:46:08 +0100

    prepare build

--- GUI/win_main.py
+++ GUI/win_main.py
@@ -8,18 +8,12 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
-    QCursor, QFont, QFontDatabase, QGradient,
-    QIcon, QImage, QKeySequence, QLinearGradient,
-    QPainter, QPalette, QPixmap, QRadialGradient,
-    QTransform)
-from PySide6.QtWidgets import (QAbstractScrollArea, QApplication, QGraphicsView, QHBoxLayout,
-                               QHeaderView, QLabel, QMainWindow, QMenu,
+from PySide6.QtCore import (QCoreApplication, QMetaObject, QRect, Qt)
+from PySide6.QtGui import (QAction, QFont)
+from PySide6.QtWidgets import (QAbstractScrollArea, QGraphicsView, QHBoxLayout,
+                               QLabel, QMenu,
                                QMenuBar, QPushButton, QSizePolicy, QSpacerItem,
-                               QStackedWidget, QTableWidget, QTableWidgetItem, QVBoxLayout,
+                               QStackedWidget, QTableWidget, QVBoxLayout,
                                QWidget, QAbstractItemView)
 
 from pyqtgraph import PlotWidget
